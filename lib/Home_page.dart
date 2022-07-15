c1d1f2bedc3619f0c47a11ae6f029740198f6d41
unknown <15014362437@139.com>
2022-07-15 09:34:18 +0800

    commit

--- lib/Home_page.dart
+++ lib/Home_page.dart
@@ -13,8 +13,8 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-      leading: Icon(Icons.back_hand),
-      title: Text("产品细节"),
+      leading: Icon(Icons.add),
+      title: Text("产品细节",),
       ),
       
       body:ListView(children:<Widget>[
@@ -22,20 +22,23 @@ class _HomePageState extends State<HomePage> {
          mainAxisAlignment: MainAxisAlignment.spaceAround,
           children: <Widget>[
             Row(children: <Widget>[
-              Text("Title",
+              Text("Widget会以矩形的中心",
               textScaleFactor: 2.5,),
-            ],
+             ],
             ),
             Row(
               crossAxisAlignment: CrossAxisAlignment.start,
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: <Widget>[
-              Icon(Icons.settings_applications,size: 80.0,),
+              Image(image:
+              AssetImage("imgs/gratisography-rainbow-wood.jpg"),
+              width: 100.0,
+              ),
               Column(children: <Widget>[
                 Text("这是一行文字",
                 textScaleFactor: 1,),
               Text("2022-07-14"),
-              ],
+               ],
               ),
               Icon(Icons.add),
             ],
@@ -43,15 +46,15 @@ class _HomePageState extends State<HomePage> {
           ],
        ),
        Image(
-         image:NetworkImage(
-         "https://unsplash.com/photos/y-VhDt6KsKg",
-         scale: 5.0
+         image:AssetImage(
+         "imgs/gratisography-rainbow-wood.jpg"
          ),
+         width: 100.0,
         ),
-      Text("7月14日,完美世界发布2022年半年度业绩预告。今年1～6月预计实现归母净利润11.1～11.6亿元,比上年同期增长331%～350%;扣除非经常性损益后的净利润为6.4～6.8亿元,同比大增1574%～1678%。其中,游戏业务实现净利润11.6～12亿元,同比增长420%～438%,游戏业务扣非后净利润约为7.2～7.6亿元,同比大增1892%～2003%。",
-      textScaleFactor: 1.0,)
-      ]
-      )
+      Text("       7月14日,完美世界发布2022年半年度业绩预告。今年1～6月预计实现归母净利润11.1～11.6亿元,比上年同期增长331%～350%;扣除非经常性损益后的净利润为6.4～6.8亿元,同比大增1574%～1678%。其中,游戏业务实现净利润11.6～12亿元,同比增长420%～438%,游戏业务扣非后净利润约为7.2～7.6亿元,同比大增1892%～2003%。",
+      textScaleFactor: 1.5,)
+        ],
+      ),
     );
   }
 }
